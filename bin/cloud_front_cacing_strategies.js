dc5871bc8b47a8689823f4b1db9956e2a94c9855
Durvesh <durvesh09@gmail.com>
2022-08-21 15:35:49 +0530

    CloudFront Demo

--- bin/cloud_front_cacing_strategies.js
+++ bin/cloud_front_cacing_strategies.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-const cdk = require('aws-cdk-lib');
+const cdk = require('@aws-cdk/core');
 const { CloudFrontCacingStrategiesStack } = require('../lib/cloud_front_cacing_strategies-stack');
+const { CloudFrontCustomCaching } = require('../lib/cloud_front_custom_cache');
 
 const app = new cdk.App();
 new CloudFrontCacingStrategiesStack(app, 'CloudFrontCacingStrategiesStack');
+
+new CloudFrontCustomCaching(app, 'CloudFrontCustomCaching');
